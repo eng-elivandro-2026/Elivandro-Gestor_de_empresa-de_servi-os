4fe3c3de833bb450ff33321b1494075a95965874
eng-elivandro-2026 <nascimento.gaube@gmail.com>
2026-04-09 23:14:19 -0300

    Delete js directory

--- js/config/supabase.js
+++
@@ -1,6 +0,0 @@
-const SUPABASE_URL = "https://ojuuzojwnyxdsavdosif.supabase.co";
-const SUPABASE_KEY = "sb_publishable_uqS8EZTOs5-sHKr1IpFhzg_eZrZOr7u";
-
-window.sbClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
-console.log("Supabase conectado!");
