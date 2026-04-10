31fcf9a510f77192f65462b35177c277e7fa2311
eng-elivandro-2026 <nascimento.gaube@gmail.com>
2026-04-10 00:22:39 -0300

    Update supabase.js

--- js/config/supabase.js
+++ js/config/supabase.js
@@ -1,11 +1,13 @@
 const SUPABASE_URL = "https://ojuuzojwnyxdsavdosif.supabase.co";
 const SUPABASE_KEY = "sb_publishable_uqS8EZTOs5-sHKr1IpFhzg_eZrZOr7u";
 
+// cria cliente global
 window.sbClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 console.log("Supabase conectado!");
 
-export async function logout(){
-  await supabase.auth.signOut();
+// logout simples (GLOBAL)
+window.logout = async function () {
+  await window.sbClient.auth.signOut();
   window.location.href = "/login.html";
-}
+};
